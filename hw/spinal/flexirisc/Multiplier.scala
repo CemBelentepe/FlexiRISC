f0828bc67b8737e861ff240b993ba631f7b3240f
Cemalettin Cem Belentepe <30960801+CemBelentepe@users.noreply.github.com>
2023-03-23 23:58:55 +0300

    Multiplier overhaul

--- hw/spinal/flexirisc/Multiplier.scala
+++ hw/spinal/flexirisc/Multiplier.scala
@@ -7,96 +7,49 @@ import spinal.lib._
 import scala.language.postfixOps
 import scala.util.Random
 
-object MUL_MODE extends SpinalEnum() {
-  val MUL, MULH, MULHS, MULHSU = newElement()
-}
-
-case class Multiplier() extends Component {
+case class Multiplier(width: Int) extends Component {
   val io = new Bundle {
-    val lhs = in Bits (32 bits)
-    val rhs = in Bits (32 bits)
+    val lhs = in UInt (width bits)
+    val rhs = in UInt (width bits)
 
-    val mode = in(MUL_MODE)
-
-    val result = out Bits (64 bits)
+    val result = out Vec(UInt (2 * width bits), log2Up(width/8) + 1)
   }
 
-  val eff_lhs = Bits(32 bits)
-  val eff_rhs = Bits(32 bits)
+  assert(width >= 8, "Width must be greater or equal to 8")
+  assert(isPow2(width), "Width must be a power of 2")
 
-  switch(io.mode) {
-    is(MUL_MODE.MUL) {
-      eff_lhs := io.lhs
-      eff_rhs := io.rhs
+  if (width <= 8) {
+    io.result(0) := io.lhs * io.rhs
+  } else {
+    val mul_ll = new Multiplier(width/2)
+    val mul_hh = new Multiplier(width/2)
+    val last_ind = log2Up(width/8)
+
+    mul_ll.io.lhs := io.lhs.subdivideIn(2 slices)(0)
+    mul_ll.io.rhs := io.rhs.subdivideIn(2 slices)(0)
+
+    mul_hh.io.lhs := io.lhs.subdivideIn(2 slices)(1)
+    mul_hh.io.rhs := io.rhs.subdivideIn(2 slices)(1)
+
+    io.result(last_ind) := ((io.lhs.subdivideIn(2 slices)(0) * io.rhs.subdivideIn(2 slices)(1)) << (width/2)) +
+      (mul_hh.io.result(last_ind-1).asBits ## mul_ll.io.result(last_ind-1).asBits).asUInt +
+      ((io.lhs.subdivideIn(2 slices)(1) * io.rhs.subdivideIn(2 slices)(0)) << (width/2))
+
+    for(i <- 0 until log2Up(width/8)){
+      io.result(i) := (mul_hh.io.result(i).asBits ## mul_ll.io.result(i).asBits).asUInt
     }
-    is(MUL_MODE.MULH) {
-      eff_lhs := io.lhs(15 downto 0).asSInt.resize(32 bits).asBits
-      eff_rhs := io.lhs(15 downto 0).asSInt.resize(32 bits).asBits
-    }
-    is(MUL_MODE.MULHS) {
-      eff_lhs := io.lhs(15 downto 0).asUInt.resize(32 bits).asBits
-      eff_rhs := io.lhs(15 downto 0).asUInt.resize(32 bits).asBits
-    }
-    is(MUL_MODE.MULHSU) {
-      eff_lhs := io.lhs(15 downto 0).asSInt.resize(32 bits).asBits
-      eff_rhs := io.lhs(15 downto 0).asUInt.resize(32 bits).asBits
-    }
-  }
-
-  val pp_0 = Vec(Vec(Bits(16 bits), 4), 4)
-
-  for (i <- 0 until 4) {
-    val a = eff_lhs.subdivideIn(8 bits)(i).asUInt
-    for (j <- 0 until 4) {
-      val b = eff_rhs.subdivideIn(8 bits)(j).asUInt
-      pp_0(i)(j) := (a * b).asBits
-    }
-  }
-
-  val pp_1 = Vec(Vec(Bits(32 bits), 2), 2)
-
-  for (i <- 0 until 2) {
-    for (j <- 0 until 2) {
-      val tr = pp_0(2 * i)(2 * j).asUInt
-      val tl = pp_0(2 * i + 1)(2 * j).asUInt
-      val br = pp_0(2 * i)(2 * j + 1).asUInt
-      val bl = pp_0(2 * i + 1)(2 * j + 1).asUInt
-
-      val top = (tl << 8) + tr
-      val btm = (bl << 8) + br
-      // val sum = (btm.asUInt + top(23 downto 8).asUInt) ## btm(7 downto 0)
-      val sum = ((btm << 8) + top)
-      pp_1(i)(j) := sum.asBits
-    }
-  }
-
-  val pp_2 = Vec(Vec(Bits(64 bits), 1), 1)
-  for (i <- 0 until 1) {
-    for (j <- 0 until 1) {
-      val tr = pp_1(2 * i)(2 * j).asUInt
-      val tl = pp_1(2 * i + 1)(2 * j).asUInt
-      val br = pp_1(2 * i)(2 * j + 1).asUInt
-      val bl = pp_1(2 * i + 1)(2 * j + 1).asUInt
-
-      val top = (tl << 16) + tr
-      val btm = (bl << 16) + br
-      val sum = ((btm << 16) + top)
-      pp_2(i)(j) := sum.asBits
-    }
-
-    io.result := pp_2(0)(0)
   }
 }
 
 object Multiplier extends App {
-  Config.spinal.generateVerilog(new Multiplier())
+  Config.spinal.generateVerilog(new Multiplier(64))
 }
 
 object MultiplierTest {
   def main(args: Array[String]): Unit = {
     SimConfig
       .compile {
-        val dut = new Multiplier()
+        val dut = new Multiplier(32)
         dut.io.simPublic()
         dut
       }
@@ -108,11 +61,10 @@ object MultiplierTest {
           val a, b = Random.nextLong().toBigInt & ((1L << 32) - 1)
           dut.io.lhs #= a
           dut.io.rhs #= b
-          dut.io.mode #= MUL_MODE.MUL
 
           sleep(10)
           val expected = a * b
-          val result = dut.io.result.toBigInt
+          val result = dut.io.result(log2Up(32/8)).toBigInt
           val passed = result == expected
 
           if (!passed) {
